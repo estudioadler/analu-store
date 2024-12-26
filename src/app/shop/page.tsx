10b02c8d5d7e89254d83583517df82ac3b7762c8
estudioadler <adler.gabriel20@gmail.com>
2024-12-26 12:08:35 -0300

    `Refactor ShopPage component refactoring: updated type and function signature`

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -5,25 +5,25 @@ import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import type { FilterParams } from '@/lib/types'
 
-export default async function ShopPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  return (
-    <>
-      <Header />
-      <div className="container mx-auto px-6 py-8">
-        <h1 className="text-3xl font-bold mb-8">Todos os Produtos</h1>
-        <Suspense fallback={<div>Carregando cabeçalho...</div>}>
-          <ShopHeader />
-        </Suspense>
-        <Suspense fallback={<div>Carregando...</div>}>
-          <ShopItems searchParams={searchParams as FilterParams} />
-        </Suspense>
-      </div>
-      <Footer />
-    </>
-  )
+type Props = {
+ params: { slug: string }
+ searchParams: { [key: string]: string | string[] | undefined }
 }
 
+export default async function ShopPage({ searchParams }: Props) {
+ return (
+   <>
+     <Header />
+     <div className="container mx-auto px-6 py-8">
+       <h1 className="text-3xl font-bold mb-8">Todos os Produtos</h1>
+       <Suspense fallback={<div>Carregando cabeçalho...</div>}>
+         <ShopHeader />
+       </Suspense>
+       <Suspense fallback={<div>Carregando...</div>}>
+         <ShopItems searchParams={searchParams as FilterParams} />
+       </Suspense>
+     </div>
+     <Footer />
+   </>
+ )
+}
